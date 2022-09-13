d40ae840c943d94fed83f626334dc47e75345b5f
S944859620 <96282264+S944859620@users.noreply.github.com>
2022-09-14 00:08:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def get_memorial_days_count(aim_date):
 
 # 各种倒计时
 def get_counter_left(aim_date):
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  next = datetime.strptime(str(date.today().year) + "-" + aim_date, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
